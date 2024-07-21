a886987d641d2e5e5aab2e2a21848d978ded532c
Victorb999 <victor.b_@hotmail.com>
2024-07-21 18:53:47 -0300

    feat: sale rotes

---
+++ src/app/api/sale/[id]/rote.ts
@@ -0,0 +1,59 @@
+import { NextResponse } from "next/server"
+import prisma from "@/lib/prisma"
+
+interface params {
+  id: string
+}
+
+export async function DELETE(req: Request, { params }: any) {
+  const id = params.id
+  try {
+    const sale = await prisma.sale.delete({
+      where: {
+        id: parseInt(id),
+      },
+    })
+    return NextResponse.json(sale)
+  } catch (error) {
+    console.error("Erro ao deletar cliente:", error)
+    return NextResponse.json({ error: "Erro ao deletar cliente" })
+  } finally {
+    await prisma.$disconnect()
+  }
+}
+
+export async function GET(req: Request, { params }: any) {
+  const id = params.id
+  try {
+    const sale = await prisma.sale.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    })
+    return NextResponse.json(sale)
+  } catch (error) {
+    console.error("Erro ao buscar cliente:", error)
+    return NextResponse.json({ error: "Erro ao buscar cliente" })
+  } finally {
+    await prisma.$disconnect()
+  }
+}
+
+export async function PUT(req: Request, { params }: any) {
+  const body = await req.json()
+  const id = params.id
+  try {
+    const sale = await prisma.sale.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: body,
+    })
+    return NextResponse.json(sale)
+  } catch (error) {
+    console.error("Erro ao atualizar cliente:", error)
+    return NextResponse.json({ error: "Erro ao atualizar cliente" })
+  } finally {
+    await prisma.$disconnect()
+  }
+}
